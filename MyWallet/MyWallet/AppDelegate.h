07af35c25f768867d32368221c9e45abae9d129a
朱昌伟 <zhuchangwei1990@163.com>
2017-05-08 22:39:54 +0800

    创建项目

---
+++ MyWallet/MyWallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWallet
+//
+//  Created by 朱昌伟 on 2017/5/8.
+//  Copyright © 2017年 zhuchangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWallet/MyWallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWallet
+//
+//  Created by 朱昌伟 on 2017/5/8.
+//  Copyright © 2017年 zhuchangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
